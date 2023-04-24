a1fc4518b3b7e1e5e23878e67246ecb6fb83f311
Raymond <ruiwenhw@gmail.com>
2023-04-23 19:39:46 -0700

    Implement arduino code

--- pathfinder_main/pathfinder_main.ino
+++ pathfinder_main/pathfinder_main.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(11, 10);
+
 #define IR_LEFT 2
 #define IR_RIGHT 13
 
@@ -8,7 +11,11 @@
 #define RIGHT_B 8
 #define RIGHT_E 9
 
-int maxSpeed = 27; // from 0 to 100
+#define TRIG 12
+#define ECHO 7
+
+const int maxSpeed = 100; // from 0 to 100
+const int distanceThreshold = 25; // an item within a range of 40 cm is considered an obstacle
 
 void setup() {
   pinMode(IR_LEFT, INPUT);
@@ -20,12 +27,35 @@ void setup() {
   pinMode(RIGHT_F, OUTPUT);
   pinMode(RIGHT_B, OUTPUT);
   pinMode(RIGHT_E, OUTPUT);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
   
   Serial.begin(9600);
+  BT.begin(9600);
 }
 
 void loop() {
-  
+  if (BT.available() > 0) {
+    char input = BT.read();
+    if (input == 'F') {
+      forwardOneUnit();
+      BT.println("Done");
+    } else if (input == 'R') {
+      rightTurn();
+      BT.println("Done");
+    } else if (input == 'L') {
+      leftTurn();
+      BT.println("Done");
+    } else if (input == 'D') { // Detect obstacle
+      float distance = getDist(); 
+      if (distance < distanceThreshold) {
+        BT.println("Yes");
+      } else {
+        BT.println("No");
+      }      
+    }
+  }
 }
 
 void forwardOneUnit() {
@@ -38,12 +68,13 @@ void forwardOneUnit() {
     } else if (digitalRead(IR_LEFT) == HIGH && digitalRead(IR_RIGHT) == LOW) {
       axialLeft();
     }  
+    delay(10);
   }
   stopCar();
 
   // drive forward a little more
   long startTime = millis();
-  int duration = 100;
+  int duration = 1000;
   while (millis() - startTime < duration) {
     forward();
   }
@@ -71,7 +102,7 @@ void leftTurn() {
                          // If the right sensor is already on the line, then count as first time done
   while (count < 2) {
     axialLeft();
-    int curState = digitalRead(IR_LEFT);
+    int curState = digitalRead(IR_RIGHT);
     if (curState == 1 && lastState == 0) {
       count++;
     }
@@ -80,9 +111,17 @@ void leftTurn() {
   stopCar();
 }
 
-void uTurn() {
-  rightTurn();
-  rightTurn();
+float getDist() {
+  long duration;
+  float distance;
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance = (duration / 2) * 0.03435;
+  return distance;
 }
 
 void motorWrite(int a, int b, int c, int d) {
