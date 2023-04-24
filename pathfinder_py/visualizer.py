e4bef5f987ebdf4aa7e0a9356983ff0142f49315
Raymond <ruiwenhw@gmail.com>
2023-04-23 19:32:21 -0700

    Improve visualizer to trace out planned path

--- pathfinder_py/visualizer.py
+++ pathfinder_py/visualizer.py
@@ -1,14 +1,40 @@
-ARROWS = ["↑", "→", "↓", "←"]
+CAR_ORIENTATION = ["▲", "▶", "▼", "◀"]
+ADJACENT = {(-1, 0): "A",
+                    (0, 1): "B",
+                    (1, 0): "C",
+                    (0, -1): "D"}
+ARROWS = {("A", "B"): "↘",
+          ("A", "C"): "↓",
+          ("A", "D"): "↙",
+          ("B", "A"): "↖",
+          ("B", "C"): "↙",
+          ("B", "D"): "←",
+          ("C", "A"): "↑",
+          ("C", "B"): "↗",
+          ("C", "D"): "↖",
+          ("D", "A"): "↗",
+          ("D", "B"): "→",
+          ("D", "C"): "↘"}
 
 
-def display_grid(grid, x, y, cur_dir):
+def display_grid(grid, cur_dir, path):
+    if not path:
+        return
+
     m, n = len(grid), len(grid[0])
     s = ""
-    for i in range(m):
-        for j in range(n):
-            if i == x and j == y:
-                s += "|" + ARROWS[cur_dir]
+    for x in range(m):
+        for y in range(n):
+            if x == path[0][0] and y == path[0][1]:
+                s += "|" + CAR_ORIENTATION[cur_dir]
+            elif x == path[-1][0] and y == path[-1][1]:
+                s += "|X"
+            elif (x, y) in path:
+                idx = path.index((x, y))
+                px, py = path[idx - 1]
+                nx, ny = path[idx + 1]
+                s += "|" + ARROWS[ADJACENT[(px - x, py - y)],ADJACENT[(nx - x, ny - y)]]
             else:
-                s += "|" + str(grid[i][j])
+                s += "|" + str(grid[x][y])
         s += "|\n"
     print(s.strip())
